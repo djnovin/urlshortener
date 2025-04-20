b9c35b969d4c4bfb4d268459515e90d80bd267e4
Novin Noori <hello@novinnoori.com>
2025-04-20 22:45:11 +1000

    Update Infra

---
+++ infra/lambda.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_function" "lambda" {
+  function_name = "${var.BASE_NAME}-lambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  runtime       = "nodejs20.x"
+  handler       = "lambda.handler"
+  filename      = "${path.module}/build/function.zip"
+  description   = "Lambda function for NestJS URL Shortener"
+
+  memory_size = 128
+  timeout     = 10
+
+  environment {
+    variables = {
+      NODE_ENV     = "production"
+      DATABASE_URL = var.DATABASE_URL
+    }
+  }
+
+  tags = local.tags
+}
